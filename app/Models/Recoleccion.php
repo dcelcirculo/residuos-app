52a811f48c94faa3a11bde992ee0242f46afa917
juan-atoy <atoy5923@gmail.com>
2025-09-28 18:01:20 -0500

    modificacion modelo recoleccion
    se agrega la definicion de la tabla creada en Bd, para que busque la tabla correcta

--- app/Models/Recoleccion.php
+++ app/Models/Recoleccion.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Recoleccion extends Model
-{
+{   
+    
     protected $fillable = ['solicitud_id','user_id','fecha_real','kilos','puntos'];
 
     public function solicitud(){
@@ -15,4 +16,7 @@ class Recoleccion extends Model
     public function recolector(){
         return $this->belongsTo(\App\Models\User::class,'user_id');
     }
+
+    // Fuerza el nombre correcto de la tabla
+    protected $table = 'recolecciones';
 }
